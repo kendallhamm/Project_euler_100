f9a87c8783cd78328869cbe820343d045948a8d6
kendallhamm <kendallhamm17@gmail.com>
2026-09-09 01:48:03 +0000

    renaming

--- 48.py
+++
@@ -1,7 +0,0 @@
-n = 1
-val = 0
-while n <= 1000:
-    val += n**n
-    n += 1
-
-print(str(val)[-10:])
--- 97.py
+++
@@ -1,3 +0,0 @@
-number = (28433 * (2 ** 7830457)) + 1
-last_10 = number % 10**10
-print(last_10)
